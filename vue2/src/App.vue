4fa20e1f5b4bac5cfb0a10a8da090562c9ee4def
FenrisX <1213791406@qq.com>
2022-05-04 21:00:57 +0800

    Initial commit

---
+++ vue2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<!--  <el-button>I am ElButton</el-button>-->
+  <div>
+<!--    <router-link to="/home">home</router-link>-->
+<!--    <router-link to="/Login">Login</router-link>-->
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import {ElButton} from "element-plus";
+
+export default {
+  name: "App",
+  components: {ElButton},
+};
+</script>
+
+<style>
+html,body,#app{
+  height: 100%;
+  margin-top: 0px;
+}
+</style>
+
---
+++ vue2/src/main.js
@@ -0,0 +1,15 @@
+import {createApp} from 'vue'
+import router from "./router";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+import axios from "axios";
+import VueAxios from 'vue-axios'
+
+
+const app = createApp(App)
+app.use(VueAxios,axios);
+app.use(ElementPlus);
+app.use(router);
+app.config.productionTip = false;
+app.mount('#app')
